4a1d83fa2199bed4908672e8445e3f5d10b590a2
Clément Aubin <aubincleme@gmail.com>
2021-12-22 15:40:35 +0100

    Allow to delete a versions #31
    * Update parser to provide prune and remove actions only in the versions
      parser (not at the top level)

--- xtool/xtool/version/parser.py
+++ xtool/xtool/version/parser.py
@@ -19,11 +19,13 @@ class VersionParser(Parser):
         # Download action
         downloadParser = subParsers.add_parser('download', aliases=['d'], help='download a new version')
         downloadParser.add_argument('version', help='the XWiki version to download')
-        # Remove action
-        removeParser = subParsers.add_parser('remove', aliases=['r'], help='remove a version')
-        removeParser.add_argument('version', help='the XWiki version to remove')
-        # Prune action
-        subParsers.add_parser('prune', aliases=['p'], help='remove any version that is not used by an instance')
+
+        if not topLevel:
+            # Remove action
+            removeParser = subParsers.add_parser('remove', aliases=['r'], help='remove a version')
+            removeParser.add_argument('version', help='the XWiki version to remove')
+            # Prune action
+            subParsers.add_parser('prune', aliases=['p'], help='remove any version that is not used by an instance')
 
     """
     See Parsers#handleArgs()
